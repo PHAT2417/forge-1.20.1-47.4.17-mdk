a5eabcbadf848fdb3d5d28d76f13e04a2b5037d7
PHAT2417 <110274715+PHAT2417@users.noreply.github.com>
2026-05-27 22:35:13 -0700

    just new

--- src/main/java/net/xuantran/tutorialmod/item/ModItems.java
+++ src/main/java/net/xuantran/tutorialmod/item/ModItems.java
@@ -12,7 +12,8 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
-    public static final RegistryObject<Item> CLAW = ITEMS.register("claw", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CLAW = ITEMS.register("claw", () 
+    -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
