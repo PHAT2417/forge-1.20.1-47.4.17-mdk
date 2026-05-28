441bd0e61b0c7beceaa4c4ced7676ea587fd19f2
PHAT2417 <110274715+PHAT2417@users.noreply.github.com>
2026-05-27 22:41:20 -0700

    new change

--- src/main/java/net/xuantran/tutorialmod/TutorialMod.java
+++ src/main/java/net/xuantran/tutorialmod/TutorialMod.java
@@ -50,7 +50,7 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(ModItems.CLAW);
+            event.accept(ModItems.MOONSHACKLE);
         }
     }
 
