6bba72760c2b2572efae6972dcd7ef4cc0d13c4b
PHAT2417 <110274715+PHAT2417@users.noreply.github.com>
2026-05-28 21:52:26 -0700

    done-temporarily

--- src/main/java/net/xuantran/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/xuantran/tutorialmod/block/ModBlocks.java
@@ -19,7 +19,7 @@ public class ModBlocks {
     DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
     public static final RegistryObject<Block> MOON_BLOCK = registerBlock("moon_block", 
-    () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)));
+    () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
