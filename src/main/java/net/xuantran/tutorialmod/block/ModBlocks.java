df808c16f21a582ecfc0a5f9443cb107ed0b0733
PHAT2417 <110274715+PHAT2417@users.noreply.github.com>
2026-05-29 22:01:21 -0700

    new ore

--- src/main/java/net/xuantran/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/xuantran/tutorialmod/block/ModBlocks.java
@@ -21,6 +21,9 @@ public class ModBlocks {
     public static final RegistryObject<Block> MOON_BLOCK = registerBlock("moon_block", 
     () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)));
 
+    public static final RegistryObject<Block> MOON_ORE = registerBlock("moon_ore", 
+    () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_ORE)));
+
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
